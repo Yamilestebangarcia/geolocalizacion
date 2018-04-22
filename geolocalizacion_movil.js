bb939ca871b41a359696fc6d29a120665452dc0a
Yamilestebangarcia <Yamil_mont@hotmail.com>
2018-04-22 20:19:07 +0200

    Update geolocalizacion_movil.js

--- geolocalizacion_movil.js
+++ geolocalizacion_movil.js
@@ -1,20 +1,17 @@
 if ("geolocation" in navigator) {
     /* la geolocalización está disponible */
-    alert("localizando");
+    //alert("localizando");
     navigator.geolocation.getCurrentPosition(
-    //navigator.geolocation.watchPosition(
-     (position) => {      
-       document.getElementById("x").value = position.coords.latitude;
-         document.getElementById("y").value=position.coords.longitude;
-         document.getElementById("exactitud").value=position.coords.accuracy;
-          document.getElementById("z").value=position.coords.altitude;
-         document.getElementById("exactitud_altura").value=position.coords.altitudeAccuracy;
-        // document.getElementById("form").submit();         
-     });
-/*uso de cookis */
- 
-  
+        (position) => {
+            document.getElementById("x").value = position.coords.latitude;
+            document.getElementById("y").value = position.coords.longitude;
+            document.getElementById("exactitud").value = position.coords.accuracy;
+            document.getElementById("z").value = position.coords.altitude;
+            document.getElementById("exactitud_altura").value = position.coords.altitudeAccuracy;
+            document.getElementById("form").submit();
+        });
 } else {
     /* la geolocalización NO está disponible */
     alert("no te puedo localizar");
+    location.href = "http: //localhost/geofoto/tarjeta.php";
 }
